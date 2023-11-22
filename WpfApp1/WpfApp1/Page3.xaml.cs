1b4e9bb79c6bad3da55d7c8cf43c50833b33ee73
BIMOPIRATSD <nacu5801@gmail.com>
2023-11-22 10:19:30 +0300

    Закончил

---
+++ WpfApp1/WpfApp1/Page3.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(!int.TryParse(Text1.Text, out int x))
+            {
+
+            }
+            int y;
+            y = (x % 7) + 1;
+            Text2.Text += $"{y}\n";
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Page4.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(!double.TryParse(Text1.Text, out double A))
+            {
+
+            }
+            if(!double.TryParse(Text2.Text, out double B))
+            {
+
+            }
+            if(!double.TryParse(Text3.Text, out double C))
+            {
+
+            }
+            double S1 = A * B;
+            double S2 = C * C;
+            double x = S1 / S2;
+            double y = S1 - x * Sqrt(C);
+            Text4.Text += $"Колличество размещённых квадратов = {x}\n, Оставшаяся площадь = {y}\n";
+        }
+    }
+}
